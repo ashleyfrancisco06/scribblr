e0b9b6007069c157cf5a4d2d9ba53b3a7279edd4
Ashley Francisco <Francisco.ashley1@gmail.com>
2019-02-04 18:40:24 -0500

    header styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './header.css';
 
 const Header = () => {
     return (
         <header className='header'>
-            <div>
-                <h1>Scribblr</h1>
-                <nav>
-                    <Link to={'/scribbls'}>Home</Link>
-                    <Link to={'/create-scribbl'}>Create Scribbl</Link>
- 
-                </nav>
-            </div>
+            <h1>Scribblr</h1>
 
+            <nav className='nav'>
+                <Link to={'/create-scribbl'}>Create Scribbl</Link>
+                <Link to={'/scribbls'}>Home</Link>
+
+            </nav>
+            <h3>A Hub for Literary Expression</h3>
         </header>
     )
 }
