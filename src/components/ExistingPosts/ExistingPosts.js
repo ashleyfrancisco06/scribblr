4ca37d7a4ec23f42555c500aa887c6cbf7dca9ed
Charlie Sheppard <sheppard.charlie24@gmail.com>
2019-02-05 14:48:43 -0500

    add selecting option to update

--- src/components/ExistingPosts/ExistingPosts.js
+++ src/components/ExistingPosts/ExistingPosts.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import Select from "react-select"
 import genres from "../../scripts/genres.json"
+import axios from 'axios'
 
 const genreOptions = genres.map((genre) => (
     {value: genre.value, label: genre.label}
     ))
+
+const selectScribbl = (e) => {
+    let scribblToUpdate = e.currentTarget.id
+    axios.get(`/scribbls/${scribblToUpdate}`)
+    .then(response => {
+        const work = response.data
+        
+        console.log(work)
+    })
+    console.log(scribblToUpdate)
+}
 
 const ExistingPosts = (props) => {
     console.log(props)
     const getCurrentScribbls = () => {
         const currentScribbls = props.searchedScribbls.map((scribbl, key) => {
             return (
-                <div key={key}>
+                <div id={scribbl.id} key={key} onClick={selectScribbl}>
                     <h1>{scribbl.title}</h1>
                     <p>{scribbl.content}</p>
                 </div>
@@ -19,6 +31,7 @@ const ExistingPosts = (props) => {
         })
         return currentScribbls
     }
+
         return (
             <div>
                 <Select
