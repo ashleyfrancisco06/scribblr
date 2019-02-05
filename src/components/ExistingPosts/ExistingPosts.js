b6242c2b5104665d75a95979defcfc87184e8641
Heather Baker <heather.nic.baker@gmail.com>
2019-02-05 16:27:44 -0500

    Passing Props Through React Router For Update

--- src/components/ExistingPosts/ExistingPosts.js
+++ src/components/ExistingPosts/ExistingPosts.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Select from "react-select"
+import {Link} from 'react-router-dom'
 import genres from "../../scripts/genres.json"
 import axios from 'axios'
 
@@ -7,26 +8,21 @@ const genreOptions = genres.map((genre) => (
     {value: genre.value, label: genre.label}
     ))
 
-const selectScribbl = (e) => {
-    let scribblToUpdate = e.currentTarget.id
-    axios.get(`/scribbls/${scribblToUpdate}`)
-    .then(response => {
-        const work = response.data
-        
-        console.log(work)
-    })
-    console.log(scribblToUpdate)
-}
+
 
 const ExistingPosts = (props) => {
     console.log(props)
     const getCurrentScribbls = () => {
         const currentScribbls = props.searchedScribbls.map((scribbl, key) => {
             return (
-                <div id={scribbl.id} key={key} onClick={selectScribbl}>
+            
+                <div id={scribbl.id} onClick={props.selectScribbl} key = {key}>
+                <Link to={{pathname: `/scribbls/${scribbl.id}`, state: scribbl}}>
                     <h1>{scribbl.title}</h1>
                     <p>{scribbl.content}</p>
+                    </Link>
                 </div>
+               
             )
         })
         return currentScribbls
